2766da3993d89612326c2d9f3f802cb90addd365
henrychu111 <henrychuuuu@gmail.com>
2021-01-13 00:21:04 -0800

    servers

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import data from '.data';
+
+const app = express();
+
+app.get("/api/books", (req,res) => {
+    res.send(data.books);
+})
+
+app.listen(5000, () => {console.log("Server started at http://localhost:5000") });
+
--- frontend/src/Screens/BookScreen.js
+++ frontend/src/Screens/BookScreen.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 import data from '../data';
 
 function BookScreen(props) {
-    console.log(props.match.params.id)
+    const [qty, setQty] = useState(1)
     const book = data.books.find(x => x._id === props.match.params.id);
     return (
         <div >
-            <div>
-                <Link to="/">Back to result</Link>
+            <div className="back-to-result">
+                <Link to="/">&lt; Back to result</Link>
             </div>
-            <div className="details"></div>
+            <div className="details">
                 <div className="details-image">
                     <img src={book.image} alt="product"></img>
                 </div>
@@ -20,11 +20,15 @@ function BookScreen(props) {
                             <h4>{book.name}</h4>
                             {book.author}
                         </li>
+                        <li className="book-type">
+                            {book.type}
+                        </li>
+                        <br></br>
                         <li>
-                            {book.rating} Stars({book.reviews} Reviews)
+                            {book.rating} Stars ({book.reviews} Reviews)
                         </li>
                         <li>
-                            <b>${book.price}</b>
+                            Price: <b>${book.price}</b>
                         </li>
                         <li>
                             Description:
@@ -33,6 +37,25 @@ function BookScreen(props) {
                             </div>
                         </li>
                     </ul>
+                </div>
+                <div className="details-action">
+                    <ul>
+                        <li>
+                            <b>Price:</b> ${book.price}
+                        </li>
+                        <li>
+                            <b>Status:</b> {book.status}
+                        </li>
+                        <li>
+                            <b>Qty:</b> &nbsp;
+                            <input type="number" min="1" max="5" value={qty} 
+                                onChange={(e) => setQty(e.target.value)}/>
+                        </li>
+                        <li>
+                            <button className="button">Add to Cart</button>
+                        </li>
+                    </ul>
+                </div>
                 </div>
             
         </div>
