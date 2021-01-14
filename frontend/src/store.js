ad32e6d58a964e8057eb95930c0c116a8335e1bc
henrychu111 <henrychuuuu@gmail.com>
2021-01-13 22:05:55 -0800

    react-redux

---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+import {applyMiddleware, combineReducers, createStore, compose} from 'redux';
+import { productListReducer } from './reducers/productReducers';
+import thunk from 'redux-thunk';
+
+const initialState= {};
+const reducer = combineReducers({
+    productList: productListReducer,    
+})
+
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+export default store;
