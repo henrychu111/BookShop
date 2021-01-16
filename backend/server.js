14831da63d252e590f0d399971b5f0abc1cd6b31
henrychu111 <henrychuuuu@gmail.com>
2021-01-16 01:21:24 -0800

    connected with moongose and made userAdmin

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoutes';
+
+dotenv.config();
+
+const mongodbURL = config.MONGODB_URL;
+mongoose.connect(mongodbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
+
+app.use("/api/users", userRoute);
 
 app.get("/api/books/:id", (req,res) => {
     const productId = req.params.id;
