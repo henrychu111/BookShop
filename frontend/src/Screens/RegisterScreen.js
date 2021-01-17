413ff725ca3c673a48099e42e2989e0d99070d33
henrychu111 <henrychuuuu@gmail.com>
2021-01-17 03:23:45 -0800

    Register page made

---
+++ frontend/src/Screens/RegisterScreen.js
@@ -0,0 +1,78 @@
+import React, {useEffect, useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {Link} from 'react-router-dom'
+import Rating from '@material-ui/lab/Rating';
+import Tooltip from '@material-ui/core/Tooltip';
+import { register } from '../actions/userActions';
+
+function RegisterScreen(props) {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [rePassword, setRePassword] = useState('')
+    const userRegister = useSelector(state => state.userRegister);
+    const {loading, userInfo, error} = userRegister;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(userInfo){
+            props.history.push('/')
+        }
+        return () => {
+          //
+        }
+    }, [userInfo]);
+
+    const submitHandler =(e) =>{
+        e.preventDefault();
+        dispatch(register(email, password));
+    }
+
+    return <div className = "form">
+        <form onSubmit={submitHandler} >
+            <ul className="form-container">
+                <li>
+                    <h3>Sign-In</h3>
+                </li>
+                <li>
+                    {loading && <div className='loading'>Loading...</div>}
+                    {error && <div className='loading'>error...</div>}
+                </li>
+                <li>
+                    <label htmlFor="name">
+                        Name
+                    </label>
+                    <input type ="name" name ="name" id ="name" onChange ={(e) => setName(e.target.value)}> 
+                    </input>
+                </li>
+                <li>
+                    <label htmlFor="email">
+                        Email
+                    </label>
+                    <input type ="email" name ="email" id ="email" onChange ={(e) => setEmail(e.target.value)}> 
+                    </input>
+                </li>
+                <li>
+                    <label htmlFor ="password">Password</label>
+                    <input type ="password" id ="password" name="password" onChange ={(e) => setPassword(e.target.value)}>
+                    </input>
+                </li>
+                <li>
+                    <label htmlFor ="rePassword">Re-enter Password</label>
+                    <input type ="rePassword" id ="rePassword" name="rePassword" onChange ={(e) => setRePassword(e.target.value)}>
+                    </input>
+                </li>
+                <li>
+                    <button type ="submit" className="button primary">Register</button>
+                </li>
+                <li>
+                    Already have an account? <Link to='/signin'>Sign-in</Link>
+                </li>
+            </ul>
+        </form>
+    </div>
+}
+
+export default RegisterScreen;
+
