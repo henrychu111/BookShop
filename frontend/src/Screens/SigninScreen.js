7db2d5d794427a76be1198899aacb66f8041eda3
henrychu111 <henrychuuuu@gmail.com>
2021-01-17 02:54:06 -0800

    sign page usable

--- frontend/src/Screens/SigninScreen.js
+++ frontend/src/Screens/SigninScreen.js
@@ -3,51 +3,61 @@ import { useDispatch, useSelector } from 'react-redux';
 import {Link} from 'react-router-dom'
 import Rating from '@material-ui/lab/Rating';
 import Tooltip from '@material-ui/core/Tooltip';
+import { signin } from '../actions/userActions';
 
 function SigninScreen(props) {
 
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
-
+    const userSignin = useSelector(state => state.userSignin);
+    const {loading, userInfo, error} = userSignin;
     const dispatch = useDispatch();
 
     useEffect(() => {
+        if(userInfo){
+            props.histroy.push('/')
+        }
         return () => {
           //
         }
-    }, []);
+    }, [userInfo]);
 
     const submitHandler =(e) =>{
         e.preventDefault();
+        dispatch(signin(email, password));
     }
 
     return <div className = "form">
         <form onSubmit={submitHandler} >
             <ul className="form-container">
                 <li>
-                    <h3>Sign In</h3>
+                    <h3>Sign-In</h3>
                 </li>
                 <li>
-                    <label for="email">
+                    {loading && <div className='loading'>Loading...</div>}
+                    {error && <div className='loading'>error...</div>}
+                </li>
+                <li>
+                    <label htmlFor="email">
                         Email
                     </label>
                     <input type ="email" name ="email" id ="email" onChange ={(e) => setEmail(e.target.value)}> 
                     </input>
                 </li>
                 <li>
-                    <label for ="password">Password</label>
+                    <label htmlFor ="password">Password</label>
                     <input type ="password" id ="password" name="password" onChange ={(e) => setPassword(e.target.value)}>
                     </input>
                 </li>
                 <li>
-                    <button type ="submit" className="button primary">Signin</button>
+                    <button type ="submit" className="button primary">Sign In</button>
                 </li>
                 <li>
                     New to BookShop?
                 </li>
                 <li>
-                    <Link to="register" className ="button full-width">Create your BookShop account</Link>
+                    <Link to="register" className ="button secondary">Create your BookShop account</Link>
                 </li>
 
             </ul>
